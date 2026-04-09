03996e0042acc561f5ca0cc608dd623fbee1be35
Almir <allo0001@stud.kea.dk>
2026-04-09 13:52:05 +0200

    Add Prettier and Tailwind CSS plugin for code formatting

--- src/app/page.js
+++ src/app/page.js
@@ -12,19 +12,25 @@ import { useState } from "react";
 export default function Home() {
   const [selectImage, setSelectImage] = useState("/navy.png");
   return (
-    <div class="grid gap-8 [grid-template-columns:[full-start]_1fr_[content-start]_minmax(0,600px)_[img-start]_minmax(0,600px)_[content-end]_1fr_[full-end]] max-w-[1400px] mx-auto">
+    <div class="mx-auto grid max-w-[1400px] [grid-template-columns:[full-start]_1fr_[content-start]_minmax(0,600px)_[img-start]_minmax(0,600px)_[content-end]_1fr_[full-end]] gap-8">
       <Header />
-      <main class="col-[content] mt-9 my-12">
-        <section class="grid grid-cols-[2fr_1fr_0.1fr] gap-8 items-center">
+      <main class="col-[content] my-12 mt-9">
+        <section class="grid grid-cols-[2fr_1fr_0.1fr] items-center gap-8">
           <div>
             <Heading />
           </div>
           <Iwatch src={selectImage} />
           <Circle
-            activeCircle={selectImage === "/navy.png" ? 1 : selectImage === "/mint.png" ? 2 : 3}
+            activeCircle={
+              selectImage === "/navy.png"
+                ? 1
+                : selectImage === "/mint.png"
+                  ? 2
+                  : 3
+            }
           />
         </section>
-        <div className="col-[content] justify-self-start text-white font-semibold text-[1.4rem] border border-white py-5 px-[70px] rounded-full">
+        <div className="col-[content] justify-self-start rounded-full border border-white px-[70px] py-5 text-[1.4rem] font-semibold text-white">
           <Btn btntext="Buy Now" />
         </div>
         <section className="grid grid-cols-[2fr_1fr] items-center gap-4 py-16">
